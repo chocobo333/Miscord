d5783882156fc4a719f0c84fcbc9160593c0facc
yamashiro-kaito <yamashiro_kaito@microad.co.jp>
2018-11-26 17:38:56 +0900

    fix greet message

--- commands/greet.js
+++ commands/greet.js
@@ -4,10 +4,9 @@ module.exports = {
     execute(message, args) {
         // 特定の言葉に反応して返答する処理(部分一致)
         if(message.content.match(/おはよ/)) {
-        let channel = message.channel
         let author = message.author.username
-        let reply_text = '寒いね'
-        message.reply(reply_text)
+        let reply_text = `${author}寒いね`
+        message.channel.reply(reply_text)
             .then(message => console.log(`Sent message: ${reply_text}`))
             .catch(console.error)
         return
